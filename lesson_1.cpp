ef4749058c06b46ca1a90cafe4e0c78a5ac71b2b
Andy <andrey-walk@yandex.ru>
2016-02-17 08:42:06 +0300

    first commit

--- lesson_1.cpp
+++ lesson_1.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+
+using namespace std;
+long int s;
+
 int main() {
-  for (int i = 1; i <= 10; i++
+  for (long int i = 1; i <= 1000000000; i++)
   {
-      
+    i = i + 1;
+    s = s + i;
+    return s;
   }
+  cout << s;
   return 0;
 }
