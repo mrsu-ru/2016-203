531165c55402f068c2957e81b2fc506e47eeee26
Nazarov <taltosh1488@gmail.com>
2016-03-09 09:06:15 +0300

    Changed n to N

--- nazarovvi.cpp
+++ nazarovvi.cpp
@@ -6,27 +6,27 @@
 void nazarovvi::lab1()
 {
     double coef = 0;
-    for (int k=0; k<n-1; k++)
+    for (int k=0; k<N-1; k++)
     {
-        for (int i=k+1; i<n; i++)
+        for (int i=k+1; i<N; i++)
         {
             coef = A[i][k]/A[k][k];
-            for (int j=k; j<n; j++)
+            for (int j=k; j<N; j++)
             {
                 A[i][j] -= coef*A[k][j];
                // if (abs(A[i][j])<1e-16) A[i][j] = 0;
             }
-            b[i] = b[i] - coef*b[k];
+            b[i] -= coef*b[k];
         }
     }
-    for(int i = 0; i<n; i++)
+    for(int i = 0; i<N; i++)
         x[i]=b[i];
 
     for (int i=n-1;i>=0;i--)
     {
-        for (int j=i+1;j<n;j++)
+        for (int j=i+1;j<N;j++)
                 x[i]-=A[i][j]*x[j];
-        x[i] = x[i] / A[i][i];
+        x[i] /= A[i][i];
     }
 
 }
