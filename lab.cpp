a8f05121acefb5eda25f0ebc45013e21f9294fa7
Ruslan Zhalnin <zhalnin@gmail.com>
2016-03-09 09:05:00 +0300

    Исправил ошибки. Добавил main.cpp

--- lab.cpp
+++ lab.cpp
@@ -4,3 +4,16 @@ void lab::read_file() {}
 void lab::run(int lab_number) {}
 void lab::write_result() {}
 void lab::check_result() {}
+
+
+/**
+ *
+ */
+void lab::init(int N, double **A, double *b, double *x)
+{
+  this->A = A;
+  this->b = b;
+  this->x = x;
+
+  this->N = N;
+}
---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <cstdlib>
+#include <cstring>
+#include "lab.h"
+#include "ivanovii.h"
+
+void print_usage();
+
+
+int main(int argc, char** argv)
+{
+  if (argc < 3) {
+    print_usage();
+    return 0;
+  }
+
+  lab *l = NULL;
+  if (strcmp(argv[1], "ivanovii")) {
+    l = new ivanovii();
+  }
+  if (strcmp(argv[1], "petrovii")) {
+    l = new ivanovii();
+  }
+  else {
+    print_usage();
+    return 0;
+  }
+
+  int n = 100;
+  double **a = new double*[n];
+  double *b = new double[n];
+  double *x = new double[n];
+  for (int i= 0; i < n; i++) {
+    a[i] = new double[n];
+  }
+
+  l->init(n, a, b, x);
+
+  for (int i = 1; i < n-1; i++) {
+    for (int j = 0; j < n; j++) {
+      if (j == i-1) {
+        a[i][j] = 1.0;
+      }
+      else if (j == i) {
+        a[i][j] = -2.0;
+      }
+      else if (j == i+1) {
+        a[i][j] = 1.0;
+      }
+      else {
+        a[i][j] = 0.0;
+      }
+    }
+  }
+
+  l->run(atoi(argv[2]));
+  l->write_result();
+  l->check_result();
+  delete l;
+  return 0;
+}
+
+
+void print_usage()
+{
+
+}
