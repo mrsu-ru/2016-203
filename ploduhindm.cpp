dd898f3fbe38083bf7fc62b73211852222db180d
Дмитрий Плодухин < >
2016-03-17 22:27:48 +0300

    LAB 1 LAB 2

--- ploduhindm.cpp
+++ ploduhindm.cpp
@@ -5,15 +5,15 @@
  */
 void ploduhindm::lab1()
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < N; i++)
         x[i] = b[i];
      long double m;
-     for (int k = 0; k < n; k++)
+     for (int k = 0; k < N-1; k++)
         {
-             for (int i = k+1; i < n; i++)
+             for (int i = k+1; i < N; i++)
              {
  				m = A[i][k] / A[k][k];
-  				for (int j = k; j < n; j++)
+  				for (int j = k; j < N; j++)
                 {
   					A[i][j]-=m * A[k][j];
   				}
@@ -22,9 +22,9 @@ void ploduhindm::lab1()
              }
         }
 
-    for (int i=n-1;i>=0;i--)
+    for (int i=N-1;i>=0;i--)
     {
-        for (int j=i+1;j<n;j++)
+        for (int j=i+1;j<N;j++)
                 x[i]-=A[i][j]*x[j];
         x[i]/=A[i][i];
     }
@@ -34,9 +34,50 @@ void ploduhindm::lab1()
 /**
  * Метод Гаусса с выбором главного элемента
  */
+ int ploduhindm::maxEl(long double* a, int x)
+    {
+        int tmp = x;
+        for (int i=x+1; i<N; i++)
+        {
+            if (abs(a[i]) > abs(a[tmp])) tmp = i;
+        }
+        return tmp;
+    }
 void ploduhindm::lab2()
 {
+    for (int i = 0; i < N; i++)
+        x[i] = b[i];
+     long double m;
+     long double* col = new long double[N];
+     for (int k = 0; k < N-1; k++)
+        {
+            for (int i=0; i<N; i++)
+            {
+                col[i] = A[i][k];
+            }
+             if (maxEl(col,k) != k)
+                {
+                swap(A[maxEl(col,k)], A[k]);
+                swap(b[maxEl(col,k)], b[k]);
+                }
+             for (int i = k+1; i < N; i++)
+             {
+ 				m = A[i][k] / A[k][k];
+  				for (int j = k; j < N; j++)
+                {
+  					A[i][j]-=m * A[k][j];
+  				}
+                 x[i]-=m * x[k];
 
+             }
+        }
+
+    for (int i=N-1;i>=0;i--)
+    {
+        for (int j=i+1;j<N;j++)
+                x[i]-=A[i][j]*x[j];
+        x[i]/=A[i][i];
+    }
 }
 
 
