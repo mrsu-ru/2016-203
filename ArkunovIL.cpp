166c0e589987e9565f606c6b61e3d3288efdb935
Аркунов Иван Л <arkunovil@lab227.local>
2016-03-25 09:12:43 +0300

    lab1_and_lab2

--- ArkunovIL.cpp
+++ ArkunovIL.cpp
@@ -1,24 +1,166 @@
-#include <stdio.h>
-#include <iostream>
-#include <math.h>
+#include "ArkunovIL.h"
+#include<fstream>
+#include<iostream>
+#include<iomanip>
 
-using namespace std;
+/**
+ * Метод Гаусса
+ */
+void ArkunovIL::lab1()
+{
+    ifstream fin("input.txt");
+  int n = 0;
+ fin>>n;
 
-int main() {
+ float m[n][n+1];
+ float x[n];
+ float s;
 
-int i;
+ for (int i=0;i<n;i++)
+    for (int j=0;j<n+1;j++)
+      fin>>m[i][j];
 
-double s1,s2;
 
-s1 = s2 = 0;
+fin.close();
 
-for(i = 1; i <= pow(10.0,9); i++)
-s1=s1+1.0/pow(i,2);
 
-for(i = pow(10.0,9); i >= 1 ; i--)
-s2=s2+1.0/pow(i,2);
+  for (int i = 0; i<n; i++)
+   for (int j=i+1;j<n+1;j++){
+    float d = m[j][i]/m[i][i];
+    for (int k=0;k<n+1;k++)
+      m[j][k]=m[j][k]-(m[i][k]*d);
+   }
 
-cout<<s1<<" - "<<s2;
+x[n]=0;
 
-return 0;
+   for(int i=n;i>=0;i--)
+     {
+      s=0;
+      for(int j=i+1;j<n;j++)
+        s=s+m[i][j]*x[j];
+      x[i]=(m[i][n]-s)/m[i][i];
+
+    }
+
+
+   ofstream fout("output.txt");
+
+   for (int j = 0; j<n; j++){
+	   fout<<x[j]<<' ';
+   }
+   fout.close();
+
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void ivanovii::lab2()
+{
+ifstream fin("input.txt");
+  int n = 0;
+ fin>>n;
+
+ float m[n][n+1];
+ float x[n];
+ float s;
+
+ for (int i=0;i<n;i++)
+    for (int j=0;j<n+1;j++)
+      fin>>m[i][j];
+
+
+fin.close();
+
+int maxi = 0;
+
+for (int i = 1; i<n; i++)
+    if (abs(m[i][0]) > abs(m[maxi][0])) maxi = i;
+
+float p;
+if (maxi != 0) {
+
+   for (int i = 0; i<n+1; i++){
+    p = m[0][i];
+    m[0][i] = m[maxi][i];
+    m[maxi][i] = p;
+   }
+}
+
+  for (int i = 0; i<n; i++)
+   for (int j=i+1;j<n+1;j++){
+    float d = m[j][i]/m[i][i];
+    for (int k=0;k<n+1;k++)
+      m[j][k]=m[j][k]-(m[i][k]*d);
+   }
+
+x[n]=0;
+
+   for(int i=n;i>=0;i--)
+     {
+      s=0;
+      for(int j=i+1;j<n;j++)
+        s=s+m[i][j]*x[j];
+      x[i]=(m[i][n]-s)/m[i][i];
+
+    }
+
+
+   ofstream fout("output.txt");
+
+   for (int j = 0; j<n; j++){
+	   fout<<x[j]<<' ';
+   }
+   fout.close();
+}
+
+
+
+/**
+ * Метод квадратного корня (метод Холецкого)
+ */
+void ivanovii::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void ivanovii::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби
+ */
+void ivanovii::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод Зейделя
+ */
+void ivanovii::lab6()
+{
+
+}
+
+
+
+/**
+ * Один из градиентных методов
+ */
+void ivanovii::lab7()
+{
+
 }
