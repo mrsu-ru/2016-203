f7a341ac3f25e3c025546460ef78a853232fd157
CheboksarovVitaly <rhm_bmn@mail.ru>
2016-04-07 22:22:50 +0300

    Update cheboksarovv.cpp

--- cheboksarovv.cpp
+++ cheboksarovv.cpp
@@ -18,10 +18,8 @@ void cheboksarovv::lab1()
        b[i]-=koefc*b[k];
       }      
      }
-     cout<<endl;
-     x[N-1]=b[N-1];
      double summa=0;
-     for(int i=N-2;i>=0;i--)
+     for(int i=N-1;i>=0;i--)
       {
        for(int j=i+1;j<N;j++){
         summa=summa+A[i][j]*x[j];}
@@ -32,7 +30,30 @@ void cheboksarovv::lab1()
 
 void cheboksarovv::lab2()
 {
-
+ double max=A[0][0],m=0,summa=0,koefc=0; 
+   int index=0;
+   for(int j=0;j<N;j++)
+    {
+     for(int i=j;i<N;i++){if (abs(A[i][j])>max) {max=abs(A[i][j]);index=i;}}
+     for(int g=0;g<N;g++) {m=A[j][g];a[j][g]=A[index][g];A[index][g]=m;m=b[j]; b[j]=b[index];b[index]=m;}    
+     max=0;
+    }
+      
+    for(int k=0;k<N;k++)
+      for(int i=k+1;i<N;i++)
+      { 
+       koefc=A[i][k];
+       for(int j=k;j<N;j++) A[i][j]-=A[k][j]*koefc/A[k][k];  
+       b[i]-=koefc*b[k]/A[k][k];
+      }      
+     for(int i=N-1;i>=0;i--)
+      {
+             
+       for(int j=i+1;j<N;j++){
+        summa=summa+A[i][j]*x[j];}
+        x[i]=(b[i]-summa)/A[i][i];
+        summa=0; 
+      }
 }
 
 void cheboksarovv::lab3()
