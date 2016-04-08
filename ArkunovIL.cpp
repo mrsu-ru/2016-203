acfba4d02191d1a3c4c00d3044f4a1faa6c00645
ArkunovIvan <arkunovil@gmail.ru>
2016-04-08 08:44:13 +0300

    lab1, lab2

--- ArkunovIL.cpp
+++ ArkunovIL.cpp
@@ -1,5 +1,4 @@
 #include "ArkunovIL.h"
-#include<fstream>
 #include<iostream>
 #include<iomanip>
 #include<cmath>
@@ -9,47 +8,32 @@
  */
 void ArkunovIL::lab1()
 {
-    ifstream fin("input.txt");
-  int n = 0;
- fin>>n;
 
- float m[n][n+1];
- float x[n];
+
+
  float s;
 
- for (int i=0;i<n;i++)
-    for (int j=0;j<n+1;j++)
-      fin>>m[i][j];
+     for (int j=0;j<N;j++)
+      A[j][n]=b[j];
 
 
-fin.close();
-
-
-  for (int i = 0; i<n; i++)
-   for (int j=i+1;j<n+1;j++){
-    float d = m[j][i]/m[i][i];
-    for (int k=0;k<n+1;k++)
-      m[j][k]=m[j][k]-(m[i][k]*d);
+  for (int i = 0; i<N; i++)
+   for (int j=i+1;j<N+1;j++){
+    float d = A[j][i]/A[i][i];
+    for (int k=0;k<N+1;k++)
+      A[j][k]=A[j][k]-(A[i][k]*d);
    }
 
-x[n]=0;
+x[N]=0;
 
-   for(int i=n;i>=0;i--)
+   for(int i=N;i>=0;i--)
      {
       s=0;
-      for(int j=i+1;j<n;j++)
-        s=s+m[i][j]*x[j];
-      x[i]=(m[i][n]-s)/m[i][i];
+      for(int j=i+1;j<N;j++)
+        s=s+A[i][j]*x[j];
+      x[i]=(A[i][N]-s)/A[i][i];
 
     }
-
-
-   ofstream fout("output.txt");
-
-   for (int j = 0; j<n; j++){
-	   fout<<x[j]<<' ';
-   }
-   fout.close();
 
 }
 
@@ -59,61 +43,44 @@ x[n]=0;
  */
 void ivanovii::lab2()
 {
-ifstream fin("input.txt");
-  int n = 0;
- fin>>n;
-
- float m[n][n+1];
- float x[n];
  float s;
 
- for (int i=0;i<n;i++)
-    for (int j=0;j<n+1;j++)
-      fin>>m[i][j];
+     for (int j=0;j<N;j++)
+      A[j][n]=b[j];
 
-
-fin.close();
 
 int maxi = 0;
 
-for (int i = 1; i<n; i++)
-    if (abs(m[i][0]) > abs(m[maxi][0])) maxi = i;
+for (int i = 1; i<N; i++)
+    if (abs(A[i][0]) > abs(A[maxi][0])) maxi = i;
 
 float p;
 if (maxi != 0) {
 
-   for (int i = 0; i<n+1; i++){
-    p = m[0][i];
-    m[0][i] = m[maxi][i];
-    m[maxi][i] = p;
+   for (int i = 0; i<N+1; i++){
+    p = A[0][i];
+    A[0][i] = A[maxi][i];
+    A[maxi][i] = p;
    }
 }
 
-  for (int i = 0; i<n; i++)
-   for (int j=i+1;j<n+1;j++){
-    float d = m[j][i]/m[i][i];
-    for (int k=0;k<n+1;k++)
-      m[j][k]=m[j][k]-(m[i][k]*d);
+  for (int i = 0; i<N; i++)
+   for (int j=i+1;j<N+1;j++){
+    float d = A[j][i]/A[i][i];
+    for (int k=0;k<N+1;k++)
+      A[j][k]=A[j][k]-(A[i][k]*d);
    }
 
-x[n]=0;
+x[N]=0;
 
-   for(int i=n;i>=0;i--)
+   for(int i=N;i>=0;i--)
      {
       s=0;
-      for(int j=i+1;j<n;j++)
-        s=s+m[i][j]*x[j];
-      x[i]=(m[i][n]-s)/m[i][i];
+      for(int j=i+1;j<N;j++)
+        s=s+A[i][j]*x[j];
+      x[i]=(A[i][N]-s)/A[i][i];
 
     }
-
-
-   ofstream fout("output.txt");
-
-   for (int j = 0; j<n; j++){
-	   fout<<x[j]<<' ';
-   }
-   fout.close();
 }
 
 
