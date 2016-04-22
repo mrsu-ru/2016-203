44f782e239695c3f02a86037bd82234243b742c1
ArkunovIvan <arkunovil@gmail.ru>
2016-04-22 08:53:53 +0300

    Third commit

--- ArkunovIL.cpp
+++ ArkunovIL.cpp
@@ -10,63 +10,71 @@ void ArkunovIL::lab1()
 {
 
  float s;
+ float W[N][N+1];
+
+  for (int i=0;i<N;i++)
+    for (int j=0;j<N;j++)
+      W[i][j] = A[i][j];
+
 
      for (int j=0;j<N;j++)
-      A[j][n]=b[j];
+      W[j][N]=b[j];
 
 
   for (int i = 0; i<N; i++)
    for (int j=i+1;j<N+1;j++){
-    float d = A[j][i]/A[i][i];
+    float d = W[j][i]/W[i][i];
     for (int k=0;k<N+1;k++)
-      A[j][k]=A[j][k]-(A[i][k]*d);
+      W[j][k]=W[j][k]-(W[i][k]*d);
    }
 
 x[N]=0;
 
-   for(int i=N-1;i>=0;i--)
+   for(int i=N;i>=0;i--)
      {
       s=0;
       for(int j=i+1;j<N;j++)
-        s=s+A[i][j]*x[j];
-      x[i]=(A[i][N]-s)/A[i][i];
-
-    }
-
+        s=s+W[i][j]*x[j];
+      x[i]=(W[i][N]-s)/W[i][i];
 }
 
 
 /**
  * Метод Гаусса с выбором главного элемента
  */
-void ivanovii::lab2()
+void ArkunovIL::lab2()
 {
  float s;
+ float W[N][N+1];
+
+  for (int i=0;i<N;i++)
+    for (int j=0;j<N;j++)
+      W[i][j] = A[i][j];
+
 
      for (int j=0;j<N;j++)
-      A[j][n]=b[j];
-
+      W[j][N]=b[j];
 
 int maxi = 0;
 
 for (int i = 1; i<N; i++)
-    if (abs(A[i][0]) > abs(A[maxi][0])) maxi = i;
+    if (abs(W[i][0]) > abs(W[maxi][0])) maxi = i;
 
 float p;
 if (maxi != 0) {
 
    for (int i = 0; i<N+1; i++){
-    p = A[0][i];
-    A[0][i] = A[maxi][i];
-    A[maxi][i] = p;
+    p = W[0][i];
+    W[0][i] = W[maxi][i];
+    W[maxi][i] = p;
    }
 }
 
   for (int i = 0; i<N; i++)
    for (int j=i+1;j<N+1;j++){
-    float d = A[j][i]/A[i][i];
+    float d = W[j][i]/W[i][i];
     for (int k=0;k<N+1;k++)
-      A[j][k]=A[j][k]-(A[i][k]*d);
+      W[j][k]=W[j][k]-(W[i][k]*d);
    }
 
 x[N]=0;
@@ -75,8 +83,8 @@ x[N]=0;
      {
       s=0;
       for(int j=i+1;j<N;j++)
-        s=s+A[i][j]*x[j];
-      x[i]=(A[i][N]-s)/A[i][i];
+        s=s+W[i][j]*x[j];
+      x[i]=(W[i][N]-s)/W[i][i];
 
     }
 }
@@ -86,7 +94,7 @@ x[N]=0;
 /**
  * Метод квадратного корня (метод Холецкого)
  */
-void ivanovii::lab3()
+void ArkunovIL::lab3()
 {
 
 }
@@ -96,7 +104,7 @@ void ivanovii::lab3()
 /**
  * Метод прогонки
  */
-void ivanovii::lab4()
+void ArkunovIL::lab4()
 {
 
 }
@@ -106,7 +114,7 @@ void ivanovii::lab4()
 /**
  * Метод Якоби
  */
-void ivanovii::lab5()
+void ArkunovIL::lab5()
 {
 
 }
@@ -116,7 +124,7 @@ void ivanovii::lab5()
 /**
  * Метод Зейделя
  */
-void ivanovii::lab6()
+void ArkunovIL::lab6()
 {
 
 }
@@ -126,7 +134,7 @@ void ivanovii::lab6()
 /**
  * Один из градиентных методов
  */
-void ivanovii::lab7()
+void ArkunovIL::lab7()
 {
 
 }
