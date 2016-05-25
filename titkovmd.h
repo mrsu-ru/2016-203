4680c3ae2e9ca1c1baaf1402e14321ebe347bfef
TitkovMihail <plushscum@yandex.ru>
2016-05-25 20:30:57 +0300

    labs 1,2,3,4,5,6

---
+++ titkovmd.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "lab.h"
+
+class titkovmd : public lab
+{
+  /**
+   * Метод Гаусса
+   */
+  virtual void lab1();
+  /**
+   * Метод Гаусса с выбором главного элемента
+   */
+  virtual void lab2();
+  /**
+   * Метод квадратного корня (метод Холецкого)
+   */
+  virtual void lab3();
+  /**
+   * Метод прогонки
+   */
+  virtual void lab4();
+  /**
+   * Метод Якоби
+   */
+  virtual void lab5();
+  /**
+   * Метод Зейделя
+   */
+  virtual void lab6();
+  /**
+   * Один из градиентных методов
+   */
+  virtual void lab7();
+
+  int maxEl(long double* a, int x);
+};
