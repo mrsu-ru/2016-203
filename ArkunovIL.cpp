4d66e4609355404cc2852ff9b722d177c11a37e9
ArkunovIvan <arkunovil@gmail.ru>
2016-05-27 08:19:31 +0300

    lab 1, lab 2, lab 3, lab 4

--- ArkunovIL.cpp
+++ ArkunovIL.cpp
@@ -93,7 +93,75 @@ x[N-1]=0;
  */
 void ArkunovIL::lab3()
 {
+    float D[N][N], min, sum;
+     float S[N][N], tS[N][N], u[N], qq[N][N];
+      S[0][0]=sqrt(A[0][0]);
+      D[0][0]=A[0][0]/A[0][0];
 
+      for (int j=1; j<N; j++)
+          S[0][j]=A[j][0]/(S[0][0]*D[0][0]);
+
+      S[1][1]=sqrt(A[1][1]-S[0][1]*S[0][1]*D[0][0]);
+      D[1][1]=(A[1][1]-S[0][1]*S[0][1]*D[0][0])/(A[1][1]-S[0][1]*S[0][1]*D[0][0]);
+
+      for (int i=2; i<N; i++)
+        S[1][i]=(A[i][1]-S[0][i]*S[0][1]*D[0][0])/(D[1][1]*S[1][1]);
+
+      for(int i=2; i<N; i++)
+      {
+          sum=0;
+          for (int k=0; k<i; k++)
+            sum=sum+S[k][i]*S[k][i]*D[k][k];
+          S[i][i]=sqrt(A[i][i]-sum);
+          D[i][i]=(A[i][i]-sum)/(A[i][i]-sum);
+
+      }
+
+
+      for(int i=1; i<N; i++)
+        for(int j=i+1; j<=N; j++)
+      {float sum=0;
+          if(i>j) min=j;
+            else min=i;
+
+
+          for (int k=0; k<=min; k++)
+            sum=sum+S[k][i]*S[k][j]*D[k][k];
+          S[i][j]=(A[j][i]-sum)/(D[i][i]*S[i][i]);
+      }
+
+      for (int i=0; i<N; i++)
+        for (int j=0; j<N; j++)
+           tS[i][j]=S[j][i];
+
+      for (int i=0; i<N; i++)
+      {
+         for (int j=0; j<N; j++)
+		 {
+			 sum=0;
+			 for (int k=0; k<N; k++)
+				sum=sum+(S[i][k]*D[k][j]);
+			 qq[i][j]=sum;
+   	     }
+      }
+
+      for (int i=0;i<N;i++)
+        for (int j=0;j<N;j++)
+        {
+           sum=0;
+           for (int k=0;k<N-1;k++)
+              sum=sum+tS[i][k]*u[k];
+           u[i]=(b[i]-sum)/tS[i][i];
+        }
+
+        for (int i=N-1;i>=0;i--)
+          for (int j=N-1;j>=0;j--)
+          {
+             sum=0;
+             for (int k=N-1;k>0;k--)
+                sum=sum+qq[i][k]*x[k];
+             x[i]=(u[i]-sum)/qq[i][i];
+          }
 }
 
 
@@ -103,6 +171,22 @@ void ArkunovIL::lab3()
  */
 void ArkunovIL::lab4()
 {
+     double* k=new double[N];
+ double* p=new double[N];
+    k[0]=-A[0][1]/A[0][0];
+    p[0]=b[0]/A[0][0];
+   for(int i=1;i<N;i++)
+    {
+     k[i]=-A[i][i+1]/(A[i][i-1]*k[i-1]+A[i][i]);
+     p[i]=(b[i]-A[i][i-1]*p[i-1])/(A[i][i-1]*k[i-1]+A[i][i]);
+    }
+    x[N-1] = p[N-1];
+    for(int i=N-1; i>=0; i--){
+     x[i] = k[i]*x[i+1]+p[i];
+    };
+      delete[] k;
+      delete[] p;
+
 
 }
 
