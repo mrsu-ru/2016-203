03f80a8795d3c4cd34babf0a0a151c14bd547970
Anton Lamaikin <alamajjkin@rambler.ru>
2016-06-01 17:12:13 +0300

    lab 3,4

--- sadovnikovev.cpp
+++ sadovnikovev.cpp
@@ -75,7 +75,44 @@ void sadovnikovev::lab2()
  */
 void sadovnikovev::lab3()
 {
+        double** L=new double*[N];
+	double qq=0;
+	for (int i=0; i<N; i++)
+	{
+		L[i]=new double[i+1];
+		qq=0;
+		for (int j=0; j<i; j++)
+		{
+			qq=0;
+			for (int k=0; k<j; k++) 
+			qq+=L[i][k]*L[j][k];
+			L[i][j]=(A[i][j]-qq)/L[j][j];
+		}
+		qq=A[i][i];
+		for (int k=0; k<i; k++)
+		 qq-=L[i][k]*L[i][k];
+		L[i][i]=sqrt(qq);
+	}
 
+	double* y=new double[N];
+
+	for (int i=0; i<N; i++)
+	{
+		for (int j=i-1; j>=0; j--) 
+		b[i]-=L[i][j]*y[j];
+		y[i]=b[i]/L[i][i];
+	}
+
+	for (int i=N-1; i>=0; i--)
+	{
+		for (int j=i+1; j<N; j++) 
+		y[i]-=L[j][i]*x[j];
+		x[i]=y[i]/L[i][i];
+	}
+	delete[] y;
+	for (int i=0; i<N; i++) delete[] L[i];
+	delete[] L;
+	return;
 }
 
 
@@ -85,7 +122,21 @@ void sadovnikovev::lab3()
  */
 void sadovnikovev::lab4()
 {
-
+ double* P=new double[N];
+ double* Q=new double[N];
+    P[0]=-A[0][1]/A[0][0];
+    Q[0]=b[0]/A[0][0];
+   for(int i=1;i<N;i++)
+    {
+     P[i]=-A[i][i+1]/(A[i][i-1]*P[i-1]+A[i][i]);
+     Q[i]=(b[i]-A[i][i-1]*Q[i-1])/(A[i][i-1]*P[i-1]+A[i][i]);
+    }
+    x[N-1] = Q[N-1];
+    for(int i=N-1; i>=0; i--){
+     x[i] = P[i]*x[i+1]+Q[i];
+    };
+      delete[] P;
+      delete[] Q;
 }
 
 
