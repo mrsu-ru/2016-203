a93e35ef6fd7ead968b97711df1094537fcc4470
Timovkinsn <nikolaewitch.sergey@yandex.ru>
2016-06-03 12:27:39 +0300

    1234
    1234

--- timovkin.cpp
+++ timovkin.cpp
@@ -32,40 +32,37 @@ x[j]=b[j]/A[j][j];
  */
 void timovkin::lab2()
 {
-   for (int i = 0; i < N; i++)
-        x[i] = b[i];
-    float m,temp;
-     int ii;
-     for (int k = 0; k < N-1; k++)
-        {
-            ii = k;
-            for (int i=k+1; i<N; i++)
-                if(abs(A[i][k]) > abs(A[ii][k])) ii = i;
-                temp=A[ii];
-                A[k]=A[ii];
-                A[k]=temp;
-                temp=0;
+  float s,c,max,l,l1;
+ int d;
+ max=abs(A[0][0]);d=0;
+ for(int i=1; i<N;i++)
+   if (abs(A[i][0])>max)
+      { max=abs(A[i][0]); d=i;};
 
-                temp=b[ii];
-                b[k]=b[ii];
-                b[k]=temp;
-             for (int i = k+1; i < N; i++)
-             {
- 				m = A[i][k] / A[k][k];
-  				for (int j = k; j < N; j++)
-                {
-  					A[i][j]-=m * A[k][j];
-  				}
-                 x[i]-=m * x[k];
+if (d!=0)
+{
+  for (int j=0;j<N;j++)
+   {
+    l=A[0][j];    A[0][j]=A[d][j];    A[d][j]=l;
+    l1=b[0];    b[0]=b[d];    b[d]=l1;
+    }
+}
 
-             }
-        }
+for (int i = 0; i<N; i++)
+ for (int k=i+1;k<N;k++)
+ {
+   c=A[k][i]/A[i][i];
+   b[k]=b[k]-b[i]*c;
+   for (int j=0;j<N;j++)
+     A[k][j]=A[k][j]-A[i][j]*c;
+ }
 
-    for (int i=N-1;i>=0;i--)
-    {
-        for (int j=i+1;j<N;j++)
-                x[i]-=A[i][j]*x[j];
-        x[i]/=A[i][i];
+ for(int i=N-1;i>=0;i--)
+     {
+      s=0;
+      for(int j=i+1;j<N;j++)
+        s=s+A[i][j]*x[j];
+      x[i]=(b[i]-s)/A[i][i];
     }
 }
 
@@ -73,45 +70,44 @@ void timovkin::lab2()
  * Метод квадратного корня (метод Холецкого)
  */
 void timovkin::lab3()
-{
-	float l[N][N], y[N][N];
-	float f[N];
-for(int i=0; i<N; i++)
-{
-		float s=0.0;
-		for(int j=0; j<i-1; j++)
-            {
-			s+=l[i][j]*l[i][j];
-		}
-		l[i][i]=sqrt(A[i][i]-s);
-		s=0.0;
-		for(int k=i; k<N; k++){
-			for(int j=0; j<i-1; j++){
-				s+=l[k][j]*l[i][j];
-			}
-			l[k][i]=(A[k][i]-s)/l[i][i];
-		}
-	}
-	for(int i=0; i<N; i++){
-		float s = 0.0;
-		for(int j=0; j<i; j++){
-			s += l[i][j]*y[j];
+{ 
+   double** L = new double*[N];
+  for (int i=0; i<N; i++)
+  L[i] = new double[N];
+  double* y = new double[N];
 
-		}
-		y[i]=(f[i]-s)/l[i][i];
-	}
-	for(int i=0; i<N; i++){
-		for(int j=0; j<i; j++){
-			std::swap(l[i][j], l[j][i]);
-		}
-	}
-   for(int i=N-1; i>=0; i--){
-		float s = 0.0;
-		for(int j=i+1; j<N; j++){
-			s += l[i][j]*x[j];
-		}
-		x[i]=(y[i]-s)/l[i][i];
-	}
+   for (int i=0; i<N; i++)
+    for (int j=0; j<N; j++)
+     {L[i][j]=0;}
+
+  for(int i=0;i<N;i++)
+  {
+   for(int k=0;k<=i-1;k++)
+    L[i][i]+=L[i][k]*L[i][k];
+   L[i][i]=sqrt(A[i][i]-L[i][i]);
+   for(int j=i+1;j<N;j++)
+   {
+    for(int k=0;k<=i-1;k++)
+     L[j][i]+=L[i][k]*L[j][k];
+    L[j][i]=(A[i][j]-L[j][i])/L[i][i];
+    }
+  }
+    double summa=0;
+     for(int i=0;i<N;i++)
+      {
+       for(int j=0;j<i;j++){
+        summa+=L[i][j]*y[j];}
+       y[i]=(b[i]-summa)/L[i][i];
+       summa=0;
+      }
+     for(int i=N-1;i>=0;i--)
+      {
+       for(int j=i+1;j<N;j++){
+        summa+=L[j][i]*x[j];}
+       x[i]=(y[i]-summa)/L[i][i];
+       summa=0;
+      }
+      delete[] y;
 
 }
 
@@ -122,7 +118,21 @@ for(int i=0; i<N; i++)
  */
 void timovkin::lab4()
 {
-
+     double* k=new double[N];
+     double* k1=new double[N];
+    k[0]=-A[0][1]/A[0][0];
+    k1[0]=b[0]/A[0][0];
+   for(int i=1;i<N;i++)
+    {
+     k[i]=-A[i][i+1]/(A[i][i-1]*k[i-1]+A[i][i]);
+     k1[i]=(b[i]-A[i][i-1]*k1[i-1])/(A[i][i-1]*k[i-1]+A[i][i]);
+    }
+    x[N-1] = k1[N-1];
+    for(int i=N-1; i>=0; i--){
+     x[i] = k[i]*x[i+1]+k1[i];
+    };
+      delete[] k;
+      delete[] k1;
 }
 
 
