6125e214d1618227542be1b82b75ba3f9c75072e
SukhovaDaria <dashik.s@mail.ru>
2016-06-03 14:08:14 +0200

    Sukhova 1,2,3.cpp

--- Sukhova.cpp
+++ Sukhova.cpp
@@ -49,7 +49,63 @@ void Sukhova::lab2()
  */
 void Sukhova::lab3()
 {
+    {
+ double **L = new double*[N];
+	for (int i = 0; i<N; i++)
+		L[i] = new double[N];
 
+	double *y = new double[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		x[i] = 0;
+		y[i] = 0;
+		for (int j = 0; j < N; j++)
+		{
+			L[i][j] = 0;
+		}
+	}
+
+	double sum = 0;
+	for (int i = 0; i<N; i++)
+	{
+		for (int k = 0; k <= i - 1; k++)
+			sum += L[i][k] * L[i][k];
+
+		L[i][i] = sqrt(A[i][i] - sum);
+		sum = 0;
+		for (int j = i + 1; j<N; j++)
+			{
+				for (int k = 0; k <= i - 1; k++)
+					sum += L[i][k] * L[j][k];
+
+				L[j][i] = (A[i][j] - sum) / L[i][i];
+				sum = 0;
+			}
+	}
+
+	for (int i = 0; i<N; i++)
+	{
+		sum = 0;
+		for (int j = 0; j<i; j++)
+			sum += L[i][j] * y[j];
+
+		y[i] = (b[i] - sum) / L[i][i];
+	}
+
+	for (int i = N - 1; i >= 0; i--)
+	{
+		sum = 0;
+		for (int j = i + 1; j<N; j++)
+			sum += L[j][i] * x[j];
+
+		x[i] = (y[i] - sum) / L[i][i];
+	}
+    delete[] y;
+	for (int i = 0; i<N; i++)
+		delete[] L[i];
+	delete[] L;
+}
 }
 
 
