5c778311dfe47c70758ec4d47ad16c7a3b8d6474
SukhovaDaria <dashik.s@mail.ru>
2016-06-03 17:02:43 +0300

    Update Sukhova.cpp

--- Sukhova.cpp
+++ Sukhova.cpp
@@ -39,6 +39,41 @@ double s=0; //s- ������������ ��� ������
  */
 void Sukhova::lab2()
 {
+//находим максимальный элемент в столбце
+for (int k = 0; k < N; k++)
+  	{
+  		double max = abs(A[k][k]);
+          int index = k;
+          for (int i = k+1; i < N; i++)
+  		{
+  			if (abs(A[i][k]) > max)
+  			{
+  				max = abs(A[i][k]);
+  				index = i;
+  			}
+  		}
+  		//меняем строчки
+  		for (int j = 0; j < N; j++) swap(A[k][j], A[index][j]);
+  		swap(b[k], b[index]);
+
+  		for (int i = k; i < N; i++)
+  		{
+  			double temp = A[i][k];
+  			if (temp == 0) continue;
+  			for (int j = 0; j < N; j++) A[i][j] = A[i][j] / temp;
+  			b[i] = b[i] / temp;
+  			if (i == k) continue;
+  			for (int j = 0; j < N; j++) {A[i][j] = A[i][j] - A[k][j];}
+
+  			b[i] = b[i] - b[k];
+
+  		}
+  	}
+  	for (int k = N-1; k >= 0; k--)
+  	{
+  		x[k] = b[k];
+  		for (int i = 0; i < k; i++) b[i] = b[i] - A[i][k] * x[k];
+  	}
 
 }
 
